028529a63f864cdd0c609710d0e1ceb1c2425b7b
Toluwani Edgal <edgalmiracle@gmail.com>
2026-08-07 01:19:25 +0100

    Finished implementing phase 1

--- libra_project/settings.py
+++ libra_project/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = []
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
