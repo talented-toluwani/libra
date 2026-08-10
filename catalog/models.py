1f5ff9c6d37c7249955f4da9d894884e36305594
Toluwani Edgal <edgalmiracle@gmail.com>
2026-08-10 03:48:26 +0100

    Implemented he skeletal part of the catalog models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey
+    total_copies = models.IntegerField()
+    availables_copies = models.IntegerField()
+    isbn = models.CharField( max_length=17, unique=True, blank= True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.title} is written by {self.author}"
+
+
