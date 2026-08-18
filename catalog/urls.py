27c7d8661846bf2f55667a643621640ba41c0490
Toluwani Edgal <edgalmiracle@gmail.com>
2026-08-18 15:53:50 +0100

    Routed the catalog views

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+
+from .views import AuthorViewSet, BookViewSet
+
+router = routers.DefaultRouter()
+router.register(r'authors', AuthorViewSet)
+router.register(r'books', BookViewSet)
+
+urlpatterns = router.urls
+
